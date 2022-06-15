e909f95e36442c0d9f958128c2eeb7fc0628764a
Leon1s <yukine970818@outlook.com>
2022-06-15 14:50:31 +0800

    feat: Update spider component

---
+++ uttu/spiders/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2022/06/09 16:15:04
+@Author  :   Leon1s
+@E-mail  :   Leon1s152427@gmail.com
+@Desc    :   Base class for Uttu Spider
+"""
+
+class BaseSpider:
+    """Base Spider"""
