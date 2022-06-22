ce8c9258a2f9cd92ba4d180c5d27b3c393c7fce1
Leon1s <yukine970818@outlook.com>
2022-06-22 16:28:37 +0800

    test: add cli test script

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,9 +1,18 @@
-import fire
+import argparse
 
-class app:
-    def main(name: str):
-        typer.echo(f"Hello {name}")
+def cli():
+    parser = argparse.ArgumentParser()
 
+    # parser.add_argument("--name", dest="str", help="Set project name")
+    # parser.add_argument("--path", dest="str", help="Set project path")
+    # parser.add_argument("--templ", dest="str", help="Using pre-built project templates")
+
+    parser.add_argument("-n", "--name", dest="name", required=True, help="Set project name")
+    parser.add_argument("-p", "--path", dest="path", help="Set project path")
+    parser.add_argument("-t", "--templ", dest="templ", choices=['sfss', 'credit'], help="Using pre-built project templates")
+
+    args = parser.parse_args(["--name", "app", "--path", "C:/", "--templ", "sfss"])
+    print(args)
 
 if __name__ == "__main__":
-    typer.run(app.main)
+    cli()
